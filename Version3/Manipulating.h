9395fb72676bcaff9b8e8dc4a413801e36ec40be
fariha <fmahmud7@myseneca.ca>
2023-12-08 12:10:06 -0500

    Third Commit Retry

---
+++ Version3/Manipulating.h
@@ -0,0 +1,13 @@
+//HEADER FILE safeguarding
+#ifndef _MANIPULATING_H_
+#define _MANIPULATING_H_
+
+//LIBRARY Declaration
+#include <stdio.h>
+#include <string.h>
+
+//Function prototype declaration for manipulating function
+void manipulating(void);
+
+
+#endif
