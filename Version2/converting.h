d36f4aba51753ff459787f9641702803d7b99da5
aalam333 <152630143+aalam333@users.noreply.github.com>
2023-12-08 22:52:01 -0500

    restoring convert files via upload

---
+++ Version2/converting.h
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+// CONVERTING.H - CONVERTING.C'S HEADER FILE
+// by Afra Alam for CPR101
+
+// defining stuff
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+
+// import libraries 
+#include <stdio.h> // for printf and user input
+#include <stdlib.h> // for convering string to int
+
+// converts string to int
+void converting(void);
+
+=======
+// CONVERTING.H - CONVERTING.C'S HEADER FILE
+// by Afra Alam for CPR101
+
+// defining stuff
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+
+// import libraries 
+#include <stdio.h> // for printf and user input
+#include <stdlib.h> // for convering string to int
+
+// converts string to int
+void converting(void);
+
+>>>>>>> 60b8b827d194b6030694505d4e37dde16cf0d6bd
+#endif
